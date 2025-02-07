9eb72dba55a66fdd44630af4b04e9643df44a37e
Diva gupta <divagupta712@gmail.com>
2025-02-07 14:34:17 +0530

    Routes added for adding item into cart , cart summary and cart clear functionality

---
+++ cart/routes/ShoppingRoutes.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const router = express.Router();
+const cartController = require("../controller/ShoppingController");
+
+//add item into cart
+router.post("/add", cartController.addItemtoCart);
+//get the summary of cart
+router.get("/summary", cartController.getCartSummary);
+//clear the cart.
+router.delete("/clear", cartController.clearCart);
