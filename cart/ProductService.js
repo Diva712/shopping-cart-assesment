fc54441d2d1f8faed3497ddc96d18d06098fe9e5
Diva gupta <divagupta712@gmail.com>
2025-02-07 12:30:42 +0530

    Fetched Product Price method is added for finding the price of product

---
+++ cart/ProductService.js
@@ -0,0 +1,18 @@
+
+const axios = require("axios");
+//Price api base url 
+const PRICE_API_URL = "http://localhost:3001/products";
+
+const fetchProductPrice = async (productName) => {
+
+  try {
+    const fetchedProduct = await axios.get(`${PRICE_API_URL}/${productName}`);
+    //return the price of fetched product;
+    return fetchProductPrice.data.price;
+  } catch (error) {
+    console.log(`Error during fetching the price for ${productName}`, error.message);
+    return null;
+  }
+}
+
+module.exports = { fetchProductPrice };
