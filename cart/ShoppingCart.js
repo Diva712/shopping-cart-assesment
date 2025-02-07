01afa7901faab990eb6a78d0a0468b6442f2c4c8
Diva gupta <divagupta712@gmail.com>
2025-02-07 13:49:08 +0530

    calculateSubtotal() , calculateTax() , calculateTotal() , displayCart() method added for cart

--- cart/ShoppingCart.js
+++ cart/ShoppingCart.js
@@ -25,7 +25,37 @@ class ShoppingCart {
     console.log(`${quantity} ${productName} added to the cart`);
   }
 
+  //calculateSubtotal 
+  calculateSubtotal() {
+    return Object.values(this.items).reduce(
+      (sum, item) => sum + item.price * item.quantity, 0
+    );
+  }
 
 
+  //calculate Tax 
+  calculateTax() {
+    //as per the github example = tax Amount / subtotal = 1.88/15.05 = 0.125 = 12.5%
+    return this.calculateSubtotal() * 0.125;
+  }
+
+  //calculate total
+  calculateTotal() {
+    return this.calculateSubtotal() + this.calculateTax();
+  }
+
+
+  displayCart() {
+    console.log("Cart Summary: ");
+    Object.entries(this.items).forEach(([product, details]) => {
+      console.log(`${details.quantity} x ${product} @ ${details.price} each`);
+    })
+
+    console.log(`Subtotal: $${this.calculateSubtotal().toFixed(2)}`);
+    console.log(`Tax(12.5%): $${this.calculateTax().toFixed(2)}`);
+    console.log(`Total: $${this.calculateTotal().toFixed(2)}`);
+  }
 
 }
+
+module.exports = ShoppingCart;
